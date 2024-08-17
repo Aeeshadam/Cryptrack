100c06250f4ce7f40438e2a56f3a2784460eb7cf
Aishat Agbolade <aishaagbolade@gmail.com>
2024-08-17 19:22:07 +0100

    removed appcache

--- contexts/ContextsProviders.tsx
+++ contexts/ContextsProviders.tsx
@@ -1,5 +1,4 @@
 import ClientProvider from "./StoreProvider";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/styles/theme";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -11,13 +10,11 @@ export default function ContextsProviders({
   children: React.ReactNode;
 }>) {
   return (
-    <AppRouterCacheProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <ClientProvider>
-          <SearchProvider>{children}</SearchProvider>
-        </ClientProvider>
-      </ThemeProvider>
-    </AppRouterCacheProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ClientProvider>
+        <SearchProvider>{children}</SearchProvider>
+      </ClientProvider>
+    </ThemeProvider>
   );
 }
